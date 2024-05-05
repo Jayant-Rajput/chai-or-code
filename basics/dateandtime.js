4e201b7d09b782ff51b3295d55a2b320dc5e6ca1
Jayant-Rajput <122915085+Jayant-Rajput@users.noreply.github.com>
2024-05-05 12:00:32 +0000

    dateandtime done

---
+++ basics/dateandtime.js
@@ -0,0 +1,17 @@
+let newdate = new Date();
+console.log(newdate);
+console.log(newdate.toDateString());
+console.log(newdate.toLocaleString());
+
+let MyCreatedDate = new Date(2024,0,26);      // 0 based indexing for month;
+console.log(MyCreatedDate);
+
+console.log(MyCreatedDate.toLocaleString());
+
+let Mydate = new Date(2005,10,13,3,30);
+console.log(Mydate.toLocaleString());
+
+
+
+
+////////////////////////////// one more topic in video :   Time Stamp /////////////////////////////////
